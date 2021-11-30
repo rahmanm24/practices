a4f1bb82f68b3f01371a30575f0e7da7fc67e122
Md Motiur Rahman <rahmanm24@roehampton.ac.uk>
2021-11-30 00:46:40 +0000

    viewsand models added

--- app/app.js
+++ app/app.js
@@ -2,22 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
 app.get("/", function(req,res){
-    res.send("Hello Nabil");
+    res.render("index");
 });
 
 app.get("/stand", function(req,res){
 
     var sql = 'SELECT BikeStand.standName, BikeStand.standID, COUNT(Bike.slID) AS count FROM BikeStand INNER JOIN Bike ON BikeStand.standID = Bike.bikeStandID WHERE Bike.bikeStatus = "Available" GROUP BY BikeStand.standID';
-    /*db.query(sql).then(results => {
-        console.log(results);
-        res.json(results);
-    });*/
+    
 
-    var output = '<table border="1px">';
+    /*var output = '<table border="1px">';
     db.query(sql).then(results => {
         for (var row of results) {
             output += '<tr>';
@@ -26,6 +26,12 @@ app.get("/stand", function(req,res){
         }
         output+= '</table>';
         res.send(output);
+    });*/
+
+    db.query(sql).then(results => {
+        // Send the results rows to the all-students template
+        // The rows will be in a variable called data
+        res.render('stands', {data: results});
     });
 
     //res.json(results);
